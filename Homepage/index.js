0f5b1ce95207bc114de2c21fbc82ddc74a879f98
Arunkumar Bhat <arunkumarbhat88@gmail.com>
2020-12-19 11:56:24 +0530

    readmore

--- Homepage/index.js
+++ Homepage/index.js
@@ -1,15 +1,9 @@
 // js for read more
-$(document).ready(function() {
-    $("#toggle").click(function() {
-      var elem = $("#toggle").text();
-      if (elem == "Read More") {
-        //Stuff to do when btn is in the read more state
-        $("#toggle").text("Read Less");
-        $("#text").slideDown();
-      } else {
-        //Stuff to do when btn is in the read less state
-        $("#toggle").text("Read More");
-        $("#text").slideUp();
-      }
-    });
+$(".rmb").click(function(){
+
+  $(this).text(function(i, v){
+     return v === 'Show More' ? 'Show Less' : 'Show More'
   });
+  });
+
+  
